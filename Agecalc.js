49c9cf3d0e19f8f8a33fa79a0d4b9ac8793b095a
Rodwan26 <rdwanbnmwsy064@gmail.com>
2024-11-19 22:35:28 +0100

    CalcAge

--- Agecalc.js
+++ Agecalc.js
@@ -2,39 +2,37 @@ const btn = document.getElementById("btn");
 const inpt = document.getElementById("inpt");
 const result = document.getElementById("result");
 
-inpt.max =  new Date().toISOString().split("T")[0];
+inpt.max = new Date().toISOString().split("T")[0];
 
+function AgeCalc() {
+  const birthDay = new Date(inpt.value);
+  d1 = birthDay.getDate();
+  m1 = birthDay.getMonth();
+  y1 = birthDay.getFullYear();
 
-
- function AgeCalc(){
-    const birthDay = new Date(inpt.value);
-d1 = birthDay.getDate();
-m1 = birthDay.getMonth();
-y1 = birthDay.getFullYear();
-
-const ToDay = new Date();
-d2 = ToDay.getDate();
-m2 = ToDay.getMonth();
-y2 = ToDay.getFullYear();
-   console.log("hi");
-  let d3 = d2 - d1;
+  const ToDay = new Date();
+  d2 = ToDay.getDate();
+  m2 = ToDay.getMonth();
+  y2 = ToDay.getFullYear();
+  console.log("hi");
+  let d3 = d2 - d1 + 1;
   let m3 = m2 - m1;
   let y3 = y2 - y1;
 
-  if(m3 < 0 ){
+  if (m3 < 0) {
     y3--;
     m3 = 12 - m2 + m1;
   }
-if(d3 < 0){
-  m3--;
-  d3 = getDayinMonth() - d1 + d2;
+  if (d3 < 0) {
+    m3--;
+    if (m3 < 0) {
+      m3 = 12;
+      y3--;
+    }
+    d3 = getDayinMonth(y1, m1) - d1 + d2;
+  }
+  function getDayinMonth(year, month) {
+    return new Date(year, month, 0).getDate();
+  }
+  result.innerHTML = `your age is ${y3} year and ${m3} month and ${d3} days`;
 }
- function getDayinMonth(year,mounth){
-  return new Date(year,mounth,0);
- }
-console.log(y3,m3,d3);
-result.innerHTML = `your age is ${y3} year and ${m3} month and ${d3} days`
-
- }
-
-
